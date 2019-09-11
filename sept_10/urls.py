c3ad820f2a7c4b3b947e6062c543a0b276c1334e
Your Name <jessemason@me.com>
2019-09-10 20:38:03 -0400

    fuck off

--- sept_10/urls.py
+++ sept_10/urls.py
@@ -19,9 +19,11 @@ from django.views.generic import *
 
 class Index(TemplateView):
     template_name= "index.html"
-
+ 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', Index.as_view(), name= 'index'),
 ]
+
+##### mason is cool
